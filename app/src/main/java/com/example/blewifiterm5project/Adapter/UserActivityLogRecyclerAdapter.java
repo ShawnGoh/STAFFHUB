a3e8502f713974f822123cea11e51a311f14caa6
JSTjoa <73066788+JSTjoa@users.noreply.github.com>
2021-03-24 13:50:55 +0800

    Merging both Mapping & Testing

--- app/src/main/java/com/example/blewifiterm5project/Adapter/UserActivityLogRecyclerAdapter.java
+++ app/src/main/java/com/example/blewifiterm5project/Adapter/UserActivityLogRecyclerAdapter.java
@@ -4,13 +4,11 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.blewifiterm5project.Models.ActivityLog;
 import com.example.blewifiterm5project.R;
 
 import java.util.ArrayList;
--- app/src/main/java/com/example/blewifiterm5project/AdminWorld/MappingFragment.java
+++ app/src/main/java/com/example/blewifiterm5project/AdminWorld/MappingFragment.java
@@ -2,14 +2,13 @@ package com.example.blewifiterm5project.AdminWorld;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.blewifiterm5project.Layout.ImageDotLayout;
 import com.example.blewifiterm5project.R;
