fd8ab3d76f8f3a24d7c8bc187e62f78a157ea4c5
sqiyan <sqiyan@gmail.com>
2021-04-05 18:10:14 +0800

    edited fingerprint algo to fit dbdatapoint object

--- app/src/main/java/com/example/blewifiterm5project/Utils/FingerprintAlgo.java
+++ app/src/main/java/com/example/blewifiterm5project/Utils/FingerprintAlgo.java
@@ -2,7 +2,11 @@ package com.example.blewifiterm5project.Utils;
 
 import android.util.Pair;
 
+import com.example.blewifiterm5project.Models.dbdatapoint;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class FingerprintAlgo {
 
@@ -14,50 +18,50 @@ public class FingerprintAlgo {
     // PAVGk means the average value of the kth processed WiFi signal strength.
     //
 
-    private ArrayList<dataPoint> dataSet = new ArrayList<>();
-    private ArrayList<dataEntry> wifiResults = new ArrayList<>();
+    private ArrayList<dbdatapoint> dataSet = new ArrayList<>();
+    private dbdatapoint wifiResults;
 
-    public FingerprintAlgo(ArrayList dataSet, ArrayList wifiResults) {
+    public FingerprintAlgo(ArrayList dataSet, dbdatapoint wifiResults) {
         // from Firebase
         this.dataSet = dataSet;
         // from wifiResults
         this.wifiResults = wifiResults;
     }
 
-    private class dataPoint {
-        String ID;
-        double X;
-        double Y;
-        ArrayList<dataEntry> APdata;
-
-        dataPoint(String ID, double X, double Y, ArrayList<dataEntry> APdata){
-            this.ID = ID;
-            this.X = X;
-            this.Y = Y;
-            this.APdata = APdata;
-        }
-    }
-
-    private class dataEntry {
-        String ID;
+//    private class dataPoint {
+//        String ID;
 //        double X;
 //        double Y;
-        String MACk;
-        double AVGk;
-        double PAVGk;
-        double DEVk;
-
-        dataEntry(String ID, double X, double Y, String MACk, double AVGk, double PAVGk, double DEVk) {
-            this.ID = ID;
+//        ArrayList<dataEntry> APdata;
+//
+//        dataPoint(String ID, double X, double Y, ArrayList<dataEntry> APdata){
+//            this.ID = ID;
 //            this.X = X;
 //            this.Y = Y;
-            this.MACk = MACk;
-            this.AVGk = AVGk;
-            this.PAVGk = PAVGk;
-            this.DEVk = DEVk;
-        }
-
-    }
+//            this.APdata = APdata;
+//        }
+//    }
+//
+//    private class dataEntry {
+//        String ID;
+////        double X;
+////        double Y;
+//        String MACk;
+//        double AVGk;
+//        double PAVGk;
+//        double DEVk;
+//
+//        dataEntry(String ID, double X, double Y, String MACk, double AVGk, double PAVGk, double DEVk) {
+//            this.ID = ID;
+////            this.X = X;
+////            this.Y = Y;
+//            this.MACk = MACk;
+//            this.AVGk = AVGk;
+//            this.PAVGk = PAVGk;
+//            this.DEVk = DEVk;
+//        }
+//
+//    }
 
     // Data received:
     // (MACk, AVGk, PAVGk, DEVk) where k = 1, 2, …, m
@@ -73,33 +77,36 @@ public class FingerprintAlgo {
         // average RSSI value received at user's location
         double total = 0;
         double FLAG = 0;
-        for (int i = 0; i < wifiResults.size(); i++) {
-            total += wifiResults.get(i).PAVGk;
-            FLAG = total/wifiResults.size();
+        double RSSI_value = 0;
+        for (HashMap.Entry<String, ArrayList<Double>> accessPoint : wifiResults.getAccesspoints().entrySet()) {
+            RSSI_value = accessPoint.getValue().get(0);
+            total += RSSI_value;
+            FLAG = total/wifiResults.getAccesspoints().size();
         }
         return FLAG;
     }
 
-    public ArrayList<dataEntry> sortMAC() {
+    public ArrayList<String> sortMAC() {
 
         double FLAG = getFLAG();
-        ArrayList<dataEntry> nearbyData = new ArrayList<>();
+        ArrayList<String> nearbyAPs = new ArrayList<>();
 
         // only include MAC address of APs near the user
-        for (int i = 0; i < wifiResults.size(); i++) {
-            if (wifiResults.get(i).PAVGk > FLAG) {
-                nearbyData.add(wifiResults.get(i));
+        for (HashMap.Entry<String, ArrayList<Double>> accessPoint : wifiResults.getAccesspoints().entrySet()) {
+            if (accessPoint.getValue().get(0) > FLAG) {
+                nearbyAPs.add(accessPoint.getKey());
             }
         }
-        return nearbyData;
+        return nearbyAPs;
     }
 
     public class positionCoordinates {
         //to interface with ZC's point setting
     }
 
-    public double getEuclideanDistance(ArrayList<dataEntry> dataPoint) {
-        ArrayList<dataEntry> nearbyData = sortMAC();
+    public double getEuclideanDistance(dbdatapoint dataPoint, dbdatapoint wifiResults) {
+        // to be run on individual datapoints in the databased
+        ArrayList<String> nearbyAPs = sortMAC(); //TODO: put into main function
 
 //        ArrayList effectiveDataSet = new ArrayList();
 //        List list = new ArrayList(Arrays.asList(effectiveDataSet)); //returns a list view of an array
@@ -109,10 +116,11 @@ public class FingerprintAlgo {
 //        }
 
         double total = 0;
-        for (int i = 0; i < dataPoint.size(); i++) {
-            for (int j = 0; j < nearbyData.size(); j++) {
-                if (nearbyData.get(j).MACk.equals(dataPoint.get(i).MACk)){
-                    total += Math.pow(Math.abs(dataPoint.get(i).PAVGk - nearbyData.get(j).PAVGk) + dataPoint.get(i).DEVk + nearbyData.get(j).DEVk, 2);
+        for (HashMap.Entry<String, ArrayList<Double>> dbaccessPoint : dataPoint.getAccesspoints().entrySet()) {
+            for (int j = 0; j < nearbyAPs.size(); j++) {
+                if (nearbyAPs.get(j).equals(dbaccessPoint.getKey())){
+                    total += Math.pow(Math.abs(wifiResults.getAccesspoints().get(nearbyAPs.get(j)).get(0) - dbaccessPoint.getValue().get(0)), 2);
+//                    total += Math.pow(Math.abs(wifiResults.getAccesspoints().get(nearbyAPs.get(j)).get(0) - dbaccessPoint.getValue().get(0)) + dataPoint.get(i).DEVk + nearbyData.get(j).DEVk, 2);
                     break;
                 }
             }
@@ -128,10 +136,10 @@ public class FingerprintAlgo {
         double sum_wy = 0;
         double sum_w = 0;
         for (int i = 0; i < dataSet.size(); i++) {
-            double di = getEuclideanDistance(dataSet.get(i).APdata);
+            double di = getEuclideanDistance(dataSet.get(i), wifiResults);
             double w = 1/di;
-            sum_wx += w*dataSet.get(i).X;
-            sum_wy += w*dataSet.get(i).Y;
+            sum_wx += w*dataSet.get(i).getCoordinates().get(0);
+            sum_wy += w*dataSet.get(i).getCoordinates().get(1);
         }
 
 //        double wj = 1 / getEuclideanDistance();
